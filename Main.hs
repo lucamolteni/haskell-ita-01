d347f7ac41d8fc03feff3d6a538b7aec373008e3
Stefano Baghino <stefano@baghino.me>
2015-07-08 21:22:00 +0200

    Added coding session output from SB, LM and FA

---
+++ Main.hs
@@ -0,0 +1,37 @@
+-- Step 1: ("Luca Molteni", "Stefano Baghino")
+-- Step 2: ("Francesco Ariis", "Stefano Baghino")
+
+module Main where
+
+import Data.List
+import Test.Hspec
+
+type InputString = String
+type CensoredWord = String
+type OutputString = String
+
+subs :: CensoredWord -> InputString -> OutputString
+subs "" rest = rest
+subs _ "" = ""
+subs censored word | censored `isPrefixOf` word = 'X' : subs (tail censored) (tail word)
+                   | otherwise = (head word) : subs censored (tail word)
+
+multisubs :: [CensoredWord] -> InputString -> OutputString
+multisubs censoreds sentence = foldl (flip subs) sentence censoreds
+
+--
+
+test :: IO ()
+test = hspec spec
+
+spec :: Spec
+spec = do
+  let blacklist = ["nice", "pony", "sun", "light", "fun", "happy", "funny", "joy"]
+  let censoring = "Such a nice day with a bright sun, makes me happy"
+  let expected = "Such a XXXX day with a bright XXX, makes me XXXXX"
+  describe "subs" $ do
+    it "replaces lbe in albero" $
+      subs "lbe" "albero" `shouldBe` "aXXXro"
+  describe "multisubs" $ do
+    it "replaces nice words" $
+      multisubs blacklist censoring `shouldBe` expected
